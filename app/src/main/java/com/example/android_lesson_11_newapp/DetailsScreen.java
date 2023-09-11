fc0065cde45639777534d439e2513fd0bb9e53d5
UsmonovAMRBEK <priensariens@gmail.com>
2023-09-11 11:26:08 +0500

    first :)

---
+++ app/src/main/java/com/example/android_lesson_11_newapp/DetailsScreen.java
@@ -0,0 +1,14 @@
+package com.example.android_lesson_11_newapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_screen);
+    }
+}
